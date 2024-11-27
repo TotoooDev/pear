353cd53806a9424d10061f103ddadc013f5a4782
TotoooDev <totooodev@proton.me>
2024-11-27 15:35:39 +0100

    add shaders

---
+++ pear/include/graphics/platform/opengl/shader.h
@@ -0,0 +1,23 @@
+#ifdef PEAR_PLATFORM_OPENGL
+
+#ifndef PEAR_GRAPHICS_OPENGL_SHADER_H_
+#define PEAR_GRAPHICS_OPENGL_SHADER_H_
+
+#include <core/types.h>
+
+typedef struct shader_t shader_t;
+
+shader_t* shader_new(const char* vertex_source, const char* fragment_source);
+void shader_delete(shader_t* shader);
+
+void shader_use(shader_t* shader);
+void shader_set_u32(shader_t* shader, u32 value, const char* name);
+void shader_set_i32(shader_t* shader, i32 value, const char* name);
+void shader_set_f32(shader_t* shader, f32 value, const char* name);
+void shader_set_vec3(shader_t* shader, vec3 value, const char* name);
+void shader_set_vec4(shader_t* shader, vec4 value, const char* name);
+void shader_set_mat4(shader_t* shader, mat4 value, const char* name);
+
+#endif
+
+#endif
---
+++ pear/src/graphics/platform/opengl/shader.c
@@ -0,0 +1,87 @@
+#ifdef PEAR_PLATFORM_OPENGL
+
+#include <graphics/platform/opengl/shader.h>
+#include <GL/glew.h>
+#include <stdlib.h>
+
+#define SHADER_INFO_LOG_LENGTH 1024
+
+typedef struct shader_t {
+    u32 id
+} shader_t;
+
+shader_t* shader_new(const char* vertex_source, const char* fragment_source) {
+    shader_t* shader = (shader_t*)malloc(sizeof(shader_t));
+
+    u32 success;
+    char info_log[SHADER_INFO_LOG_LENGTH];
+    u32 vertex_shader, fragment_shader;
+    
+    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_source, NULL);
+    glCompileShader(vertex_shader);
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertex_shader, SHADER_INFO_LOG_LENGTH, NULL, info_log);
+        PEAR_ERROR("failed to compile vertex shader! opengl error: %s", info_log);
+    }
+
+    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_source, NULL);
+    glCompileShader(fragment_shader);
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragment_shader, SHADER_INFO_LOG_LENGTH, NULL, info_log);
+        PEAR_ERROR("failed to compile fragment shader! opengl error: %s", info_log);
+    }
+
+    shader->id = glCreateProgram();
+    glAttachShader(shader->id, vertex_shader);
+    glAttachShader(shader->id, fragment_shader);
+    glLinkProgram(shader->id);
+    glGetProgramiv(shader->id, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shader->id, SHADER_INFO_LOG_LENGTH, NULL, info_log);
+        PEAR_ERROR("failed to link shader! opengl error: %s", info_log);
+    }
+
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+
+    return shader;
+}
+
+void shader_delete(shader_t* shader) {
+    glDeleteProgram(shader->id);
+    free(shader);
+}
+
+void shader_use(shader_t* shader) {
+    glUseProgram(shader->id);
+}
+
+void shader_set_u32(shader_t* shader, u32 value, const char* name) {
+    glUniform1ui(glGetUniformLocation(shader->id, name), value);
+}
+
+void shader_set_i32(shader_t* shader, i32 value, const char* name) {
+    glUniform1i(glGetUniformLocation(shader->id, name), value);
+}
+
+void shader_set_f32(shader_t* shader, f32 value, const char* name) {
+    glUniform1f(glGetUniformLocation(shader->id, name), value);
+}
+
+void shader_set_vec3(shader_t* shader, vec3 value, const char* name) {
+    glUniform3fv(glGetUniformLocation(shader->id, name), 1, value);
+}
+
+void shader_set_vec4(shader_t* shader, vec4 value, const char* name) {
+    glUniform4fv(glGetUniformLocation(shader->id, name), 1, value);
+}
+
+void shader_set_mat4(shader_t* shader, mat4 value, const char* name) {
+    glUniformMatrix4fv(glGetUniformLocation(shader->id, name), 1, false, (f32*)value);
+}
+
+#endif
