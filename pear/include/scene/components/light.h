08cc5481428842df77d05ad714785bb94be46bcc
TotoooDev <totooodev@proton.me>
2024-12-14 15:55:22 +0100

    add light component

---
+++ pear/include/scene/components/light.h
@@ -0,0 +1,11 @@
+#ifndef PEAR_SCENE_COMPONENT_LIGHT_H_
+#define PEAR_SCENE_COMPONENT_LIGHT_H_
+
+#include <graphics/light.h>
+
+typedef struct light_component_t {
+    light_t light;
+    bool cast;
+} light_component_t;
+
+#endif
