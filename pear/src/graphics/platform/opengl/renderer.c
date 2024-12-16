16cbcca1fd37f78082e4ba80c1854090be850df3
TotoooDev <totooodev@proton.me>
2024-12-16 12:11:45 +0100

    add render list

--- pear/src/graphics/platform/opengl/renderer.c
+++ pear/src/graphics/platform/opengl/renderer.c
@@ -14,6 +14,7 @@
 #include <scene/components/transform.h>
 #include <scene/components/model.h>
 #include <scene/components/camera.h>
+#include <scene/components/light.h>
 #include <event/event_dispatcher.h>
 #include <util/filesystem.h>
 #include <util/array.h>
@@ -34,6 +35,11 @@ typedef struct renderer_t {
     f32 viewport_scale_y;
     f32 viewport_width_scaled;
     f32 viewport_height_scaled;
+
+    array_t* models;
+    array_t* lights;
+    array_t* model_transforms;
+    array_t* light_transforms;
 
     mat4 projection_matrix;
     mat4 view_matrix;
@@ -115,6 +121,15 @@ void renderer_setup_debug_output() {
 #endif
 }
 
+void renderer_init_ubo_matrices(renderer_t* renderer) {
+    ubo_info_t* info = uboinfo_new();
+    uboinfo_add_mat4(info);
+    uboinfo_add_mat4(info);
+    uboinfo_add_mat4(info);
+    renderer->ubo_matrices = ubo_new(info, true);
+    uboinfo_delete(info);
+}
+
 void renderer_draw_mesh(renderer_t* renderer, mesh_t* mesh , material_t material, mat4 model_matrix) {
     shader_set_i32(renderer->shader, 0, "u_texture");
     ubo_set_mat4(renderer->ubo_matrices, 0, model_matrix);
@@ -131,14 +146,9 @@ void renderer_handle_model(renderer_t* renderer, entity_t* entity) {
     if (!model->draw) {
         return;
     }
-    mat4 model_matrix;
-    transformcomponent_get_model_matrix(transform, model_matrix);
 
-    for (u32 j = 0; j < model_get_num_meshes(model->model); j++) {
-        mesh_t* mesh = model_get_meshes(model->model)[j];
-        material_t material = model_get_materials(model->model)[mesh_get_material_index(mesh)];
-        renderer_draw_mesh(renderer, mesh, material, model_matrix);
-    }
+    array_add(renderer->models, model->model);
+    array_add(renderer->model_transforms, transform);
 }
 
 void renderer_handle_camera(renderer_t* renderer, entity_t* entity) {
@@ -147,7 +157,36 @@ void renderer_handle_camera(renderer_t* renderer, entity_t* entity) {
 
     if (camera->use) {
         camera_get_view_matrix(transform->pos, transform->rotation[0], transform->rotation[1], transform->rotation[2], renderer->view_matrix);
-        ubo_set_mat4(renderer->ubo_matrices, 1, renderer->view_matrix);
+    }
+}
+
+void renderer_handle_light(renderer_t* renderer, entity_t* entity) {
+    transform_component_t* transform = (transform_component_t*)entity_get_component(entity, ENTITY_COMPONENT_TRANSFORM);
+    light_component_t* light = (light_component_t*)entity_get_component(entity, ENTITY_COMPONENT_LIGHT);
+
+    if (!light->cast) {
+        return;
+    }
+
+    array_add(renderer->lights, &light->light);
+    array_add(renderer->light_transforms, transform);
+}
+
+void renderer_render(renderer_t* renderer) {
+    ubo_set_mat4(renderer->ubo_matrices, 1, renderer->view_matrix);
+
+    for (u32 i = 0; i < array_get_length(renderer->models); i++) {
+        model_t* model = array_get(renderer->models, i);
+        transform_component_t* transform = array_get(renderer->model_transforms, i);
+
+        mat4 model_matrix;
+        transformcomponent_get_model_matrix(transform, model_matrix);
+
+        for (u32 j = 0; j < model_get_num_meshes(model); j++) {
+            mesh_t* mesh = model_get_meshes(model)[j];
+            material_t material = model_get_materials(model)[mesh_get_material_index(mesh)];
+            renderer_draw_mesh(renderer, mesh, material, model_matrix);
+        }
     }
 }
 
@@ -172,14 +211,13 @@ renderer_t* renderer_new() {
     renderer->viewport_scale_y = window_get_scale_y(app_get_window());
     renderer->viewport_width_scaled = renderer->viewport_width * window_get_scale_x(app_get_window());
     renderer->viewport_height_scaled = renderer->viewport_height * window_get_scale_y(app_get_window());
+    renderer->lights = array_new(10);
+    renderer->models = array_new(10);
+    renderer->light_transforms = array_new(10);
+    renderer->model_transforms = array_new(10);
     renderer->shader = shader_new(fileystem_read_file("shaders/shader.vert"), fileystem_read_file("shaders/shader.frag"));
 
-    ubo_info_t* info = uboinfo_new();
-    uboinfo_add_mat4(info);
-    uboinfo_add_mat4(info);
-    uboinfo_add_mat4(info);
-    renderer->ubo_matrices = ubo_new(info, true);
-    uboinfo_delete(info);
+    renderer_init_ubo_matrices(renderer);
 
     glm_mat4_identity(renderer->view_matrix);
 
@@ -199,13 +237,14 @@ void renderer_clear(renderer_t* renderer, f32 r, f32 g, f32 b) {
     glEnable(GL_DEPTH_TEST);
     glClearColor(r, g, b, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    array_clear(renderer->lights);
+    array_clear(renderer->models);
+    array_clear(renderer->light_transforms);
+    array_clear(renderer->model_transforms);
 }
 
 void renderer_draw_scene(renderer_t* renderer, scene_t* scene) {
-    shader_use(renderer->shader);
-    shader_set_u32(renderer->shader, 0, "u_platform");
-    shader_set_ubo(renderer->shader, renderer->ubo_matrices, "ubo_matrices");
-
     for (u32 i = 0; i < array_get_length(scene_get_entities(scene)); i++) {
         entity_t* entity = array_get(scene_get_entities(scene), i);
         if (entity_has_component(entity, ENTITY_COMPONENT_MODEL) && entity_has_component(entity, ENTITY_COMPONENT_TRANSFORM)) {
@@ -215,7 +254,16 @@ void renderer_draw_scene(renderer_t* renderer, scene_t* scene) {
         if (entity_has_component(entity, ENTITY_COMPONENT_CAMERA) && entity_has_component(entity, ENTITY_COMPONENT_TRANSFORM)) {
             renderer_handle_camera(renderer, entity);
         }
+
+        if (entity_has_component(entity, ENTITY_COMPONENT_LIGHT) && entity_has_component(entity, ENTITY_COMPONENT_TRANSFORM)) {
+            renderer_handle_light(renderer, entity);
+        }
     }
+    
+    shader_use(renderer->shader);
+    shader_set_u32(renderer->shader, 0, "u_platform");
+    shader_set_ubo(renderer->shader, renderer->ubo_matrices, "ubo_matrices");
+    renderer_render(renderer);
 }
 
 #endif
