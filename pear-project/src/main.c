c046f4038c7a23e34a3d38dda937a2a947a48cba
TotoooDev <totooodev@proton.me>
2024-12-16 16:20:38 +0100

    test lights

--- pear-project/src/main.c
+++ pear-project/src/main.c
@@ -8,12 +8,26 @@
 #include <model_entity.h>
 #include <camera_entity.h>
 
+#include <scene/components/light.h>
+#include <scene/components/transform.h>
+
 int main(int argc, char* argv[]) {
     app_init();
 
     scene_t* scene = app_get_scene();
     modelentity_create(scene);
     cameraentity_create(scene);
+
+    entity_t* entity = scene_add_entity(scene, "light", ENTITY_COMPONENT_TRANSFORM, ENTITY_COMPONENT_LIGHT, ENTITY_COMPONENT_END);
+
+    light_component_t* light = (light_component_t*)entity_get_component(entity, ENTITY_COMPONENT_LIGHT);
+    light->cast = true;
+    light->light.ambient[0] = 1.0f;
+    light->light.ambient[2] = 1.0f;
+
+    transform_component_t* transform = entity_get_component(entity, ENTITY_COMPONENT_TRANSFORM);
+    transform->pos[0] = 1.0f;
+    transform->pos[2] = 1.0f;
 
     gui_add(gui_info, NULL);
     gui_add(gui_scene_inspector, NULL);
--- pear/assets/shaders/shader.frag
+++ pear/assets/shaders/shader.frag
@@ -1,14 +1,40 @@
 #version 330 core
 
+#define NUM_MAX_LIGHTS 128
+
 out vec4 frag_color;
+
+struct light_t {
+    uint type;
+
+    vec3 pos;
+    vec3 direction;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+
+    float constant;
+    float linear;
+    float quadratic;
+
+    float cutoff;
+    float outer_cutoff;
+};
 
 in VS_OUT {
     vec2 texture_coords;
 } fs_in;
 
+layout (std140) uniform ubo_lights {
+    uint u_num_lights;
+    light_t u_lights[NUM_MAX_LIGHTS];
+};
+
 uniform sampler2D u_texture;
 
 void main() {
-    frag_color = texture(u_texture, fs_in.texture_coords);
+    frag_color = texture(u_texture, fs_in.texture_coords) * vec4(u_lights[0].ambient, 1.0);
     // frag_color = vec4(fs_in.texture_coords, 0.0, 1.0);
+    // frag_color = vec4(u_lights[0].pos, 1.0);
 }
