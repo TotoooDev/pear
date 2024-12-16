1fcd8ecee8b58ec42ef83e808dcc3486adfff19c
TotoooDev <totooodev@proton.me>
2024-12-16 11:51:15 +0100

    add pear_realloc

--- pear/include/core/alloc.h
+++ pear/include/core/alloc.h
@@ -5,6 +5,7 @@
 
 void* pear_malloc(size_t size);
 void pear_free(void* data);
+void* pear_realloc(void* data, size_t size);
 
 #ifndef PEAR_MALLOC
     #define PEAR_MALLOC(size) pear_malloc(size)
@@ -12,6 +13,10 @@ void pear_free(void* data);
 
 #ifndef PEAR_FREE
     #define PEAR_FREE(data) pear_free(data)
+#endif
+
+#ifndef PEAR_REALLOC
+    #define PEAR_REALLOC(data, size) pear_realloc(data, size)
 #endif
 
 #endif
--- pear/src/core/alloc.c
+++ pear/src/core/alloc.c
@@ -14,3 +14,7 @@ void* pear_malloc(size_t size) {
 void pear_free(void* data) {
     free(data);
 }
+
+void* pear_realloc(void* data, size_t size) {
+    return realloc(data, size);
+}
