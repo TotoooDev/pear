8f5e4c7111dcaee2e5ebc26d933f8044d90f4582
TotoooDev <totooodev@proton.me>
2024-12-22 19:10:27 +0100

    reorganize scene

--- pear-project/src/entities.c
+++ pear-project/src/entities.c
@@ -1,12 +1,16 @@
-#include <camera_entity.h>
 #include <scene/components/transform.h>
 #include <scene/components/script.h>
+#include <scene/components/model.h>
+#include <scene/components/light.h>
+#include <scene/components/skybox.h>
 #include <event/event_dispatcher.h>
 #include <event/keyboard.h>
 #include <graphics/camera.h>
-
+#include <graphics/image.h>
+#include <graphics/cubemap.h>
 #include <graphics/platform/opengl/window.h>
 #include <core/app.h>
+#include <core/log.h>
 
 static struct {
     bool forward;
@@ -118,7 +122,7 @@ void cameraentity_on_update(entity_t* entity, f32 timestep) {
     }
 }
 
-entity_t* cameraentity_create(scene_t* scene) {
+void cameraentity_create(scene_t* scene) {
     entity_t* entity = scene_add_entity(scene, "camera", ENTITY_COMPONENT_TRANSFORM, ENTITY_COMPONENT_CAMERA, ENTITY_COMPONENT_SCRIPT, ENTITY_COMPONENT_END);
 
     transform_component_t* transform = (transform_component_t*)entity_get_component(entity, ENTITY_COMPONENT_TRANSFORM);
@@ -129,6 +133,84 @@ entity_t* cameraentity_create(scene_t* scene) {
     script->on_update = cameraentity_on_update;
 
     event_subscribe(cameraentity_on_event, entity);
+}
 
-    return entity;
+void modelentity_create(scene_t* scene) {
+    entity_t* entity = scene_add_entity(scene, "entity", ENTITY_COMPONENT_MODEL, ENTITY_COMPONENT_TRANSFORM, ENTITY_COMPONENT_END);
+
+    bool success;
+    pear_model_t model = pear_model_load("sponza.model", &success);
+    if (!success) {
+        PEAR_ERROR("failed to load model sponza.model!");
+        return;
+    }
+
+    model_component_t* model_comp = (model_component_t*)entity_get_component(entity, ENTITY_COMPONENT_MODEL);
+    model_comp->model = model_new_from_pear_model(model, TEXTURE_WRAPPING_REPEAT, TEXTURE_FILTERING_LINEAR);
+    pear_model_free(&model);
+
+    transform_component_t* transform = (transform_component_t*)entity_get_component(entity, ENTITY_COMPONENT_TRANSFORM);
+    transform->scale[0] = 0.01f;
+    transform->scale[1] = 0.01f;
+    transform->scale[2] = 0.01f;
+}
+
+void lightentity_create(scene_t* scene) {
+    light_component_t* light;
+    transform_component_t* transform;
+
+    entity_t* directional = scene_add_entity(scene, "light", ENTITY_COMPONENT_TRANSFORM, ENTITY_COMPONENT_LIGHT, ENTITY_COMPONENT_END);
+    light = (light_component_t*)entity_get_component(directional, ENTITY_COMPONENT_LIGHT);
+    light->light.type = LIGHT_TYPE_DIRECTIONAL;
+    transform = entity_get_component(directional, ENTITY_COMPONENT_TRANSFORM);
+    transform->rotation[0] = 0.1f;
+    transform->rotation[1] = -1.0f;
+
+    entity_t* point_1 = scene_add_entity(scene, "point 1", ENTITY_COMPONENT_TRANSFORM, ENTITY_COMPONENT_LIGHT, ENTITY_COMPONENT_END);
+    light = (light_component_t*)entity_get_component(point_1, ENTITY_COMPONENT_LIGHT);
+    light->light.type = LIGHT_TYPE_POINT;
+    transform = entity_get_component(point_1, ENTITY_COMPONENT_TRANSFORM);
+    transform->pos[0] = 9.0f;
+    transform->pos[1] = 3.0f;
+    transform->pos[2] = -3.0f;
+
+    entity_t* point_2 = scene_add_entity(scene, "point 2", ENTITY_COMPONENT_TRANSFORM, ENTITY_COMPONENT_LIGHT, ENTITY_COMPONENT_END);
+    light = (light_component_t*)entity_get_component(point_2, ENTITY_COMPONENT_LIGHT);
+    light->light.type = LIGHT_TYPE_POINT;
+    transform = entity_get_component(point_2, ENTITY_COMPONENT_TRANSFORM);
+    transform->pos[0] = -7.0f;
+    transform->pos[1] = 2.0f;
+    transform->pos[2] = -3.0f;
+
+    entity_t* point_3 = scene_add_entity(scene, "point 3", ENTITY_COMPONENT_TRANSFORM, ENTITY_COMPONENT_LIGHT, ENTITY_COMPONENT_END);
+    light = (light_component_t*)entity_get_component(point_3, ENTITY_COMPONENT_LIGHT);
+    light->light.type = LIGHT_TYPE_POINT;
+    transform = entity_get_component(point_3, ENTITY_COMPONENT_TRANSFORM);
+    transform->pos[0] = 3.0f;
+    transform->pos[1] = 4.0f;
+    transform->pos[2] = 4.0f;
+}
+
+void skyboxentity_create(scene_t* scene) {
+    entity_t* skybox_entity = scene_add_entity(scene, "skybox", ENTITY_COMPONENT_SKYBOX, ENTITY_COMPONENT_END);
+
+    bool success;
+    pear_image_t right = pear_image_load("skybox/right.image", &success);
+    pear_image_t left = pear_image_load("skybox/left.image", &success);
+    pear_image_t top = pear_image_load("skybox/top.image", &success);
+    pear_image_t bottom = pear_image_load("skybox/bottom.image", &success);
+    pear_image_t front = pear_image_load("skybox/front.image", &success);
+    pear_image_t back = pear_image_load("skybox/back.image", &success);
+
+    image_t* images[] = {
+        image_new_from_pear_image(right),
+        image_new_from_pear_image(left),
+        image_new_from_pear_image(top),
+        image_new_from_pear_image(bottom),
+        image_new_from_pear_image(front),
+        image_new_from_pear_image(back)
+    };
+
+    skybox_component_t* skybox = entity_get_component(skybox_entity, ENTITY_COMPONENT_SKYBOX);
+    skybox->cubemap = cubemap_new_from_images(images, TEXTURE_WRAPPING_CLAMP, TEXTURE_FILTERING_LINEAR);
 }
--- pear-project/src/main.c
+++ pear-project/src/main.c
@@ -6,13 +6,7 @@
 #include <graphics/gui/info.h>
 #include <core/app.h>
 
-#include <model_entity.h>
-#include <camera_entity.h>
-
-#include <scene/components/light.h>
-#include <scene/components/transform.h>
-#include <scene/components/skybox.h>
-#include <pear_image.h>
+#include <entities.h> 
 
 int main(int argc, char* argv[]) {
     app_init();
@@ -20,37 +14,8 @@ int main(int argc, char* argv[]) {
     scene_t* scene = app_get_scene();
     modelentity_create(scene);
     cameraentity_create(scene);
-
-    entity_t* entity = scene_add_entity(scene, "light", ENTITY_COMPONENT_TRANSFORM, ENTITY_COMPONENT_LIGHT, ENTITY_COMPONENT_END);
-
-    light_component_t* light = (light_component_t*)entity_get_component(entity, ENTITY_COMPONENT_LIGHT);
-    light->light.type = LIGHT_TYPE_DIRECTIONAL;
-
-    transform_component_t* transform = entity_get_component(entity, ENTITY_COMPONENT_TRANSFORM);
-    transform->rotation[0] = 0.1f;
-    transform->rotation[1] = -1.0f;
-
-    entity_t* skybox_entity = scene_add_entity(scene, "skybox", ENTITY_COMPONENT_SKYBOX, ENTITY_COMPONENT_END);
-
-    bool success;
-    pear_image_t right = pear_image_load("skybox/right.image", &success);
-    pear_image_t left = pear_image_load("skybox/left.image", &success);
-    pear_image_t top = pear_image_load("skybox/top.image", &success);
-    pear_image_t bottom = pear_image_load("skybox/bottom.image", &success);
-    pear_image_t front = pear_image_load("skybox/front.image", &success);
-    pear_image_t back = pear_image_load("skybox/back.image", &success);
-
-    image_t* images[] = {
-        image_new_from_pear_image(right),
-        image_new_from_pear_image(left),
-        image_new_from_pear_image(top),
-        image_new_from_pear_image(bottom),
-        image_new_from_pear_image(front),
-        image_new_from_pear_image(back)
-    };
-
-    skybox_component_t* skybox = entity_get_component(skybox_entity, ENTITY_COMPONENT_SKYBOX);
-    skybox->cubemap = cubemap_new_from_images(images, TEXTURE_WRAPPING_CLAMP, TEXTURE_FILTERING_LINEAR);
+    lightentity_create(scene);
+    skyboxentity_create(scene);
 
     gui_add(gui_info, NULL);
     gui_add(gui_renderer, NULL);
