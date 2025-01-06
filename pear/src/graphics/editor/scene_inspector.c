74d105c30fae4ee72bd248d8ad1ffc3e8a782706
TotoooDev <totooodev@proton.me>
2025-01-06 13:48:35 +0100

    add remove entity button

--- pear/src/graphics/editor/scene_inspector.c
+++ pear/src/graphics/editor/scene_inspector.c
@@ -7,6 +7,7 @@
 #include <graphics/editor/cimgui/cimgui.h>
 
 static scene_t* editor_scene = NULL;
+static entity_t* editor_selected_entity = NULL;
 
 void editor_set_scene(scene_t* scene) {
     editor_scene = scene;
@@ -26,14 +27,26 @@ void editor_scene_inspector(bool* show) {
         if (igButton("add entity", (ImVec2){ 0.0f, 0.0f })) {
             scene_add_entity(editor_scene, "new entity", ENTITY_COMPONENT_END);
         }
+        igSameLine(0.0f, 8.0f);
+        if (igButton("remove entity", (ImVec2){ 0.0f, 0.0f })) {
+            scene_remove_entity(editor_scene, editor_selected_entity);
+            editor_set_entity(NULL);
+            editor_selected_entity = NULL;
+        }
 
         igSeparator();
         for (u32 i = 0; i < array_get_length(entities); i++) {
             entity_t* entity = (entity_t*)array_get(entities, i);
+
+            ImGuiTreeNodeFlags flags = ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_SpanAvailWidth | ImGuiTreeNodeFlags_FramePadding;
+            if (entity == editor_selected_entity) {
+                flags |= ImGuiTreeNodeFlags_Selected;
+            }
             
-            if (igTreeNodeEx_Str(entity_get_name(entity), ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_SpanAvailWidth | ImGuiTreeNodeFlags_FramePadding)) {
+            if (igTreeNodeEx_Str(entity_get_name(entity), flags)) {
                 if (igIsItemClicked(ImGuiMouseButton_Left)) {
                     editor_set_entity(entity);
+                    editor_selected_entity = entity;
                 }
 
                 igTreePop();
