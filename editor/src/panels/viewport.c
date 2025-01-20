20d0f961cee5403c858b7530f55d0e4f63241ace
TotoooDev <totooodev@proton.me>
2025-01-20 18:14:48 +0100

    fix viewport vertically flipped

--- editor/src/panels/viewport.c
+++ editor/src/panels/viewport.c
@@ -15,7 +15,7 @@ void panel_viewport() {
         texture_t* texture = renderer_get_screen_texture(app_get_renderer());
 
         #ifdef PEAR_PLATFORM_OPENGL
-            igImage(texture_get_id(texture), size, (ImVec2){ 0.0f, 0.0f }, (ImVec2){ 1.0f, 1.0f }, (ImVec4){ 1.0f, 1.0f, 1.0f, 1.0f }, (ImVec4){ 0.0f, 0.0f, 0.0f, 0.0f });
+            igImage(texture_get_id(texture), size, (ImVec2){ 0.0f, 1.0f }, (ImVec2){ 1.0f, 0.0f }, (ImVec4){ 1.0f, 1.0f, 1.0f, 1.0f }, (ImVec4){ 0.0f, 0.0f, 0.0f, 0.0f });
         #endif
     }
     igEnd();
