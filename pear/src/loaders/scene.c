9cb6a3b87a4fa0dc959350f0b1a56f86c89d44a9
TotoooDev <totooodev@proton.me>
2025-01-21 19:26:27 +0100

    add component to scene loader

--- pear/src/loaders/scene.c
+++ pear/src/loaders/scene.c
@@ -1,6 +1,11 @@
 #include <loaders/scene.h>
+#include <loaders/model.h>
 #include <loaders/vendor/cJSON.h>
 #include <scene/components/transform.h>
+#include <scene/components/model.h>
+#include <scene/components/camera.h>
+#include <scene/components/lua_script.h>
+#include <scene/components/light.h>
 #include <util/filesystem.h>
 #include <core/log.h>
 #include <core/alloc.h>
@@ -49,7 +54,58 @@ scene_t* loader_load_scene(const char* filename) {
             loader_parse_vec3(json_rotation, transform->rotation);
             loader_parse_vec3(json_scale, transform->scale);
         }
+
+        if (cJSON_HasObjectItem(json_components, "model")) {
+            model_component_t* model = entity_add_component(entity, ENTITY_COMPONENT_MODEL);
+
+            cJSON* json_model = cJSON_GetObjectItem(json_components, "model");
+            model->draw = cJSON_IsTrue(cJSON_GetObjectItem(json_model, "draw"));
+            model->shadow_caster = cJSON_IsTrue(cJSON_GetObjectItem(json_model, "shadow_caster"));
+
+            char* model_path = cJSON_GetStringValue(cJSON_GetObjectItem(json_model, "model_path"));
+            model->model = loader_load_gltf(model_path);
+        }
+
+        if (cJSON_HasObjectItem(json_components, "camera")) {
+            camera_component_t* camera = entity_add_component(entity, ENTITY_COMPONENT_CAMERA);
+
+            cJSON* json_cam = cJSON_GetObjectItem(json_components, "camera");
+            camera->use = cJSON_IsTrue(cJSON_GetObjectItem(json_cam, "use"));
+        }
+
+        if (cJSON_HasObjectItem(json_components, "lua_script")) {
+            lua_script_component_t* script = entity_add_component(entity, ENTITY_COMPONENT_LUA_SCRIPT);
+
+            cJSON* json_script = cJSON_GetObjectItem(json_components, "lua_script");
+            script->run = cJSON_IsTrue(cJSON_GetObjectItem(json_script, "run"));
+
+            char* script_path = cJSON_GetStringValue(cJSON_GetObjectItem(json_script, "script_path"));
+            script->script = script_new_from_file(script_path, entity);
+        }
+
+        if (cJSON_HasObjectItem(json_components, "light")) {
+            light_component_t* light_comp = entity_add_component(entity, ENTITY_COMPONENT_LIGHT);
+            light_t light;
+
+            cJSON* json_light = cJSON_GetObjectItem(json_components, "light");
+
+            light.type = (u32)cJSON_GetNumberValue(cJSON_GetObjectItem(json_light, "type"));
+            loader_parse_vec3(cJSON_GetObjectItem(json_light, "ambient"), light.ambient);
+            loader_parse_vec3(cJSON_GetObjectItem(json_light, "diffuse"), light.diffuse);
+            loader_parse_vec3(cJSON_GetObjectItem(json_light, "specular"), light.specular);
+            light.constant = cJSON_GetNumberValue(cJSON_GetObjectItem(json_light, "constant"));
+            light.linear = cJSON_GetNumberValue(cJSON_GetObjectItem(json_light, "linear"));
+            light.quadratic = cJSON_GetNumberValue(cJSON_GetObjectItem(json_light, "quadratic"));
+            light.cutoff = cJSON_GetNumberValue(cJSON_GetObjectItem(json_light, "cutoff"));
+            light.outer_cutoff = cJSON_GetNumberValue(cJSON_GetObjectItem(json_light, "outer_cutoff"));
+
+            light_comp->cast = cJSON_IsTrue(cJSON_GetObjectItem(json_light, "cast"));
+            light_comp->shadow_caster = cJSON_IsTrue(cJSON_GetObjectItem(json_light, "shadow_caster"));
+            light_comp->light = light;
+        }
     }
+
+    cJSON_Delete(json);
 
     return scene;
 }
